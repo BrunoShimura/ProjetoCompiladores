2bf854bde555754e2359654e8fc70ebfff396924
BrunoShimura <bruno_shimura@hotmail.com>
2020-08-19 08:03:56 -0300

    token

--- main.js
+++ main.js
@@ -1,7 +1,18 @@
 function compilar(cod){
-  var a = cod.split('');
+  var a = cod.split('\n');
   for(var i = 0, len = a.length; i < len; ++i) {
     console.log(a[i]);
   }
+  percorre();
 }
-
+function percorre(){
+  var a = d3.csv("data/simbolos.csv").then(function(data) {
+      var a = data[0];
+      
+      console.log(a.classificacao);
+      bd(a.simbolo);
+  });
+}
+function bd(a){
+  console.log(a);
+}
