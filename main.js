d4f6f87e82d3262a2047a3aab599921687d7f199
BrunoShimura <bruno_shimura@hotmail.com>
2020-08-31 11:09:52 -0300

    lista

--- main.js
+++ main.js
@@ -13,15 +13,46 @@ function percorre(linha,num){
       for(var i = 0, len = palavra.length; i < len; ++i) {
         for(var j = 0, a = data.length; j < a; ++j) {
           if(palavra[i]==data[j].token){
-            console.log(palavra[i]);
-            console.log(data[j].simbolo);
-          }else{
-            var caracter = palavra[i].split('');
-            if (caracter[i]==data[j].token){
-
-            }
+            //console.log(num);
+            //console.log(palavra[i]);
+            //console.log(data[j].simbolo);
           }
         }
       }
   });
 }
+function lista(){
+  let head = null;
+  let length = 0;
+  const Node = (value) => {
+    return{
+      value,
+      next:null
+    }
+  }
+  const add = (value) => {
+    if(!head){
+      head = Node(value)
+      length++;
+      return head;
+    }
+    let node = head
+    while(node.next){
+      node = node.next
+    }
+    node.next = Node(value)
+    length++;
+    return node.next
+  }
+  return{
+    length: () => length,
+    add: (value) => add(value),
+    print: () => console.log(head)
+  }
+}
+const list = lista();
+console.log(list.length())
+list.add((1,1))
+list.add(2,4)
+console.log(list.length())
+console.log(list.print())
