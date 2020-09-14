1d7e209817f53df7f24bff87f730483ee138c397
BrunoShimura <bruno_shimura@hotmail.com>
2020-09-13 22:23:24 -0300

    reconhecer-cadeia

--- main.js
+++ main.js
@@ -1,27 +1,24 @@
 var cont = 0
-var teste = []
+var cadeia = []
 
-async function compilar(cod){
+function compilar(cod){
   var linha = cod.split('\n'); 
   var cont2 = 1;
- 
   for(var i = 0, len = linha.length; i < len; ++i) {
     percorre(linha[i],cont2);
     cont2+=1;
   }
-  
-  await mostraTabela(teste)
-  
+  mostraTabela(cadeia)
+  reconhecer(cadeia)
 }
 
-async function percorre(linha,num){
+function percorre(linha,num){
   d3.csv("data/simbolos.csv").then(function(data) {
       var palavra = linha.split(' ');
-    
       for(var i = 0, len = palavra.length; i < len; ++i) {
         for(var j = 0, a = data.length; j < a; ++j) {
           if(palavra[i]==data[j].token){
-            teste[cont] = [num,palavra[i],data[j].simbolo]
+            cadeia[cont] = [num,palavra[i],data[j].simbolo]
             cont+=1
           }
         }
@@ -29,11 +26,27 @@ async function percorre(linha,num){
   });
 }
 
+
+function reconhecer(cadeia){
+  d3.csv("data/tabela.csv").then(function(data) {
+    var tabela = []
+    tabela[0] = [['$','E'],cadeia,['E','->','T','S']]
+    mostraTabela2(tabela)
+});
+}
+
 function mostraTabela(tokens){
   var text = ''
   for (var i=0 ; i< tokens.length; i++){
     text+= '<tr><td>'+tokens[i][0]+'</td><td>'+tokens[i][1]+'</td><td>'+tokens[i][2]+'</td></tr>'
   }
-
   document.getElementById("tabela1").innerHTML = text;
+}
+
+function mostraTabela2(tokens){
+  var text = ''
+  for (var i=0 ; i< tokens.length; i++){
+    text+= '<tr><td>'+tokens[i][0]+'</td><td>'+tokens[i]+'</td><td>'+tokens[i][2]+'</td></tr>'
+  }
+  document.getElementById("tabela2").innerHTML = text;
 }
