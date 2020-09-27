80012c36cbd0cc69be3ea2eb9f6cabeaafcf4e39
BrunoShimura <bruno_shimura@hotmail.com>
2020-09-27 15:59:28 -0300

    pilha

--- main.js
+++ main.js
@@ -1,5 +1,7 @@
 var cont = 0
 var cadeia = []
+var pilha = ['$','E']
+//a,b,c,d,e,f,g,h,i,j
 
 function compilar(cod){
   var linha = cod.split('\n'); 
@@ -11,7 +13,6 @@ function compilar(cod){
   }
   mostraTabela(cadeia)
   reconhecer(cadeia)
-  
 }
 
 function percorre(linha,num){
@@ -29,21 +30,6 @@ function percorre(linha,num){
   });
 }
 
-
-function reconhecer(cadeia){
-  var teste = []
-  for(var i=0;i<cadeia.length;i++){
-    teste[i] = cadeia[i][1]
-  }
-  
-  d3.csv("data/tabela.csv").then(function(data) {
-    var tabela = []
-    tabela[0] = [['$E'],teste.join(''),['E->TS']]
-    mostraTabela2(tabela)
-    console.log(data[2])
-});
-}
-
 function mostraTabela(tokens){
   var text = ''
   for (var i=0 ; i< tokens.length; i++){
@@ -59,4 +45,54 @@ function mostraTabela2(tokens){
     text+= '<tr><td>'+tokens[i][0]+'</td><td>'+tokens[i][1]+'</td><td>'+tokens[i][2]+'</td></tr>'
   }
   document.getElementById("tabela2").innerHTML = text;
+}
+function reconhecer(vetorCadeia){
+  d3.csv("data/tabela.csv").then(function(data) {
+    var matriz = [[]];
+    for(var i=0;i<3;i++){
+      matriz[i] = [];
+      for(var j=0;j<4;j++){
+        matriz[i][j] = 0;
+      }
+    }
+    
+    for(var j=0;j<data.length;j++){
+      matriz[j] = [];
+    }
+    for(var j=0;j<data.length;j++){
+      matriz[j][0] = data[j].a;
+    }
+    for(var j=0;j<data.length;j++){
+      matriz[j][1] = data[j].b;
+    }
+    for(var j=0;j<data.length;j++){
+      matriz[j][2] = data[j].c;
+    }
+    for(var j=0;j<data.length;j++){
+      matriz[j][3] = data[j].d;
+    }
+    for(var j=0;j<data.length;j++){
+      matriz[j][4] = data[j].i;
+    }
+    for(var j=0;j<data.length;j++){
+      matriz[j][5] = data[j].j;
+    }
+    
+    var vetorCadeia = []
+    for(var i=0;i<cadeia.length;i++){
+      vetorCadeia[i] = cadeia[i][1]
+    }
+  
+    
+    console.log(pilha)
+    console.log(vetorCadeia)
+    console.log(matriz)
+    var teste = pilha.length
+    var tabela = []
+    for(var i=0;i<10;i++){
+      tabela[i] = [pilha,vetorCadeia,matriz]
+    }
+    console.log(pilha[pilha.length-1])
+    mostraTabela2(tabela)
+  });
 }
