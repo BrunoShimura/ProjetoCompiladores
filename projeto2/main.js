8834e49a4ab0d691057ed3ab9da0096f526b4e48
BrunoShimura <bruno_shimura@hotmail.com>
2020-11-15 17:21:53 -0300

    reconhecendo-tokens

--- projeto2/main.js
+++ projeto2/main.js
@@ -1,8 +1,6 @@
 var cont = 0
 var cadeia = []
 cadeia[0] = ['']
-var pilha = ['$','E']
-//a,b,c,d,e,f,g,h,i,j
 
 function compilar(cod){
   var linha = cod.split('\n'); 
@@ -11,33 +9,18 @@ function compilar(cod){
     percorre(linha[i],cont2);
     cont2+=1;
   }
-
-  document.getElementById("btn1").style.display = "none";
+  reconhecer()
+  
+  //document.getElementById("btn1").style.display = "none";
   document.getElementById("line_numbers").disabled = true;
 }
 
 function percorre(linha,num){
-  d3.csv("data/simbolos.csv").then(function(data) {
-      var palavra = linha.split(' ');
-      for(var i = 0, len = palavra.length; i < len; ++i) {
-        for(var j = 0, a = data.length; j < a; ++j) {
-          if(palavra[i]==data[j].token){
-            cadeia[cont] = [num,palavra[i],data[j].simbolo]
-            cont+=1
-          }
-        }
-      }
-      mostraTabela(cadeia)
-  });
-}
-
-function mostraTabela(tokens){
-  var text = ''
-  for (var i=0 ; i< tokens.length; i++){
-    text+= '<tr><td>'+tokens[i][0]+'</td><td>'+tokens[i][1]+'</td><td>'+tokens[i][2]+'</td></tr>'
-  }
-  document.getElementById("tabela1").innerHTML = text;
-  //document.getElementById("btn1").style.display = "none";
+  var palavra = linha.split(' ');
+  for(var i = 0, len = palavra.length; i < len; ++i) {
+      cadeia[cont] = [num,palavra[i]]
+      cont+=1
+    }
 }
 
 function mostraTabela2(tokens){
@@ -55,31 +38,129 @@ function novoBotao(){
 function atualizar(){
   window.location.href = "index.html";
 }
+//==============================================================================
 
-function reconhecer(vetorCadeia){
 
-}
+
+function reconhecer(){
+  cont = 0
+  contTab = 0
+  aux = 0
+  var tabela = []
+  var variaveis = []
+  for (var i=0 ; i< cadeia.length;){
+    console.log(cadeia[i][1]);
+
+//  0 ponto ====================================================================
+//  INPP - main()
+    if(cadeia[i][1]=="main"){
+      tabela[contTab] =  ["","INPP",""]
+      contTab++
+    }
+//  0 ponto ====================================================================
+//  CRCT - carrega 0
+//  ARMZ - armazena 1
+
+    for(var j=0 ; j < variaveis.length;j++){
+      if(cadeia[i][1]==variaveis[j]){
+        if(cadeia[i+1][1] == "="&&cadeia[i+3][1] == ";"){
+          console.log(cadeia[i+2][1]);
+          tabela[contTab] =  ["","CRCT",cadeia[i+2][1]]
+          contTab++
+          tabela[contTab] =  ["","ARMZ",j]
+          contTab++
+          i+=3
+        }
+      }
+    }
+
+
 //  1 ponto ====================================================================
 //  AMEM n - aloca espaço na memória (pilha M) para n variáveis.
+    if(cadeia[i][1]=="int"){
+      i++
+      while (cadeia[i][1] !== ";"){
+        if (cadeia[i][1] != ",")
+          variaveis.push(cadeia[i][1])
+        i++
+      }
+      tabela[contTab] =  ["","AMEM",variaveis.length]
+      contTab++
+    }
 //  DMEM n - desaloca o espaço das n variáveis alocadas em AMEM
-
+    if(cadeia[i][1]=="int"){
+      console.log("AMEM")
+    }
 //  1 ponto ====================================================================
 //  LEIT - scanf(a) scanf(b)
-
+    if(cadeia[i][1]=="scanf"){
+      while (cadeia[i+2][1] !== variaveis[aux]){
+        aux++
+      }
+      tabela[contTab] =  ["","LEIT",""]
+      contTab++
+      tabela[contTab] =  ["","ARMZ",aux]
+      contTab++
+    }
 //  2 pontos ===================================================================
 //  IMPR - printf(a)
-
+    if(cadeia[i][1]=="printf"){
+      console.log("IMPR")
+    }   
 //  2 pontos ===================================================================
 //  Expressão
 //  SOMA (Somar)
+    if(cadeia[i][1]=="+"){
+      console.log("SOMA")
+    }
 //  SUBT (Subtração)
+    if(cadeia[i][1]=="-"){
+      console.log("SUBT")
+    }
 //  MULT (Multiplicação)
+    if(cadeia[i][1]=="*"){
+      console.log("MULT")
+    }
 //  DIVI (Divisão)
-
+    if(cadeia[i][1]=="/"){
+      console.log("DIVI")
+    }
 //  2 pontos ===================================================================
 //  IF com ELSE
 //  DSVF p - desvia para p se topo for falso; (p é endereço)
 //  DSVS q - desvia sempre para q (q é endereço)
-        
+    if(cadeia[i][1]=="if"){
+      console.log("DSVF")
+    }
 //  2 pontos ===================================================================
 //  WHILE
+    if(cadeia[i][1]=="while"){
+      tabela[contTab] =  ["L1","NADA",""]
+      contTab++
+
+      aux = 0
+      while (cadeia[i+1][1] !== variaveis[aux]){
+        aux++
+      }
+      tabela[contTab] =  ["","CRCT",aux]
+      contTab++
+
+      aux = 0
+      while (cadeia[i+3][1] !== variaveis[aux]){
+        aux++
+      }
+      tabela[contTab] =  ["","CRCT",aux]
+      contTab++
+
+      if(cadeia[i+2][1]=="<="){
+        tabela[contTab] =  ["","CMEG",""]
+        contTab++
+      }
+      tabela[contTab] =  ["","DSVF","L2"]
+      contTab++
+    }
+
+    i++
+  }
+  mostraTabela2(tabela)
+}
