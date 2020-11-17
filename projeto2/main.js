655cdad759829e672c96d077367208cd210165f5
BrunoShimura <bruno_shimura@hotmail.com>
2020-11-16 22:17:31 -0300

    finalizado

--- projeto2/main.js
+++ projeto2/main.js
@@ -46,6 +46,9 @@ function reconhecer(){
   cont = 0
   contTab = 0
   aux = 0
+  varWhile=0
+  fim=null
+  fimElse=null
   var tabela = []
   var variaveis = []
   for (var i=0 ; i< cadeia.length;){
@@ -64,12 +67,58 @@ function reconhecer(){
     for(var j=0 ; j < variaveis.length;j++){
       if(cadeia[i][1]==variaveis[j]){
         if(cadeia[i+1][1] == "="&&cadeia[i+3][1] == ";"){
-          console.log(cadeia[i+2][1]);
-          tabela[contTab] =  ["","CRCT",cadeia[i+2][1]]
+          aux=0
+          
+          for(var g=0  ; g < variaveis.length;g++){
+            if(cadeia[i+2][1]==variaveis[g]){
+              aux=g
+            }
+          }
+          if(aux>=1){
+              tabela[contTab] =  ["","CRCT",aux]
+              contTab++
+              tabela[contTab] =  ["","ARMZ",j]
+              contTab++
+              i+=3
+          }else{
+            tabela[contTab] =  ["","CRCT",cadeia[i+2][1]]
+            contTab++
+            tabela[contTab] =  ["","ARMZ",j]
+            contTab++
+            i+=3
+          }
+        }
+        if(cadeia[i+1][1] == "="&& cadeia[i+3][1] == "+"){
+          for(var g=0 ; g < variaveis.length;g++){
+            if(cadeia[i+2][1]==variaveis[g]){
+              tabela[contTab] =  ["","CRCT",g]
+              contTab++
+            }
+          }
+          aux=null
+          
+          for(var g=0  ; g < variaveis.length;g++){
+            if(cadeia[i+4][1]==variaveis[g]){
+              aux=g
+            }
+          }
+          if(aux!=null){
+            tabela[contTab] =  ["","CRCT",aux]
+            contTab++
+          }else{
+            tabela[contTab] =  ["","CRVL",cadeia[i+4][1]]
+            contTab++
+          }
+          tabela[contTab] =  ["","SOMA",""]
           contTab++
-          tabela[contTab] =  ["","ARMZ",j]
-          contTab++
-          i+=3
+
+          for(var g=0 ; g < variaveis.length;g++){
+            if(cadeia[i][1]==variaveis[g]){
+              tabela[contTab] =  ["","ARMZ",g]
+              contTab++
+            }
+          }
+          i+=5
         }
       }
     }
@@ -88,9 +137,7 @@ function reconhecer(){
       contTab++
     }
 //  DMEM n - desaloca o espaço das n variáveis alocadas em AMEM
-    if(cadeia[i][1]=="int"){
-      console.log("AMEM")
-    }
+
 //  1 ponto ====================================================================
 //  LEIT - scanf(a) scanf(b)
     if(cadeia[i][1]=="scanf"){
@@ -105,36 +152,70 @@ function reconhecer(){
 //  2 pontos ===================================================================
 //  IMPR - printf(a)
     if(cadeia[i][1]=="printf"){
-      console.log("IMPR")
+      aux=null
+      for(var g=0  ; g < variaveis.length;g++){
+        if(cadeia[i+2][1]==variaveis[g]){
+          aux=g
+        }
+      }
+      if(aux!=null){
+        tabela[contTab] =  ["","CRCT",aux]
+        contTab++
+        tabela[contTab] =  ["","IMPR",""]
+        contTab++
+      }else{
+        tabela[contTab] =  ["","CRVL",cadeia[i+2][1]]
+        contTab++
+        tabela[contTab] =  ["","IMPR",""]
+        contTab++
+      }
+      i+=2
     }   
 //  2 pontos ===================================================================
 //  Expressão
-//  SOMA (Somar)
-    if(cadeia[i][1]=="+"){
-      console.log("SOMA")
-    }
-//  SUBT (Subtração)
-    if(cadeia[i][1]=="-"){
-      console.log("SUBT")
-    }
-//  MULT (Multiplicação)
-    if(cadeia[i][1]=="*"){
-      console.log("MULT")
-    }
-//  DIVI (Divisão)
-    if(cadeia[i][1]=="/"){
-      console.log("DIVI")
-    }
+
 //  2 pontos ===================================================================
 //  IF com ELSE
 //  DSVF p - desvia para p se topo for falso; (p é endereço)
 //  DSVS q - desvia sempre para q (q é endereço)
     if(cadeia[i][1]=="if"){
-      console.log("DSVF")
+      aux = 0
+      while (cadeia[i+1][1] !== variaveis[aux]){
+        aux++
+      }
+      tabela[contTab] =  ["","CRCT",aux]
+      contTab++
+
+      aux = 0
+      while (cadeia[i+3][1] !== variaveis[aux]){
+        aux++
+      }
+      tabela[contTab] =  ["","CRCT",aux]
+      contTab++
+
+      if(cadeia[i+2][1]=="<="){
+        tabela[contTab] =  ["","CMEG",""]
+        contTab++
+      }
+      if(cadeia[i+2][1]==">"){
+        tabela[contTab] =  ["","CMMA",""]
+        contTab++
+      }
+      
+      tabela[contTab] =  ["","DSVF","L3"]
+      contTab++
+    }
+    if(cadeia[i][1]=="else"){
+      fimElse=1
+      tabela[contTab] =  ["","DSVS","L4"]
+      contTab++
+      tabela[contTab] =  ["L3","NADA",""]
+      contTab++
     }
 //  2 pontos ===================================================================
 //  WHILE
     if(cadeia[i][1]=="while"){
+      varWhile=1
       tabela[contTab] =  ["L1","NADA",""]
       contTab++
 
@@ -156,9 +237,39 @@ function reconhecer(){
         tabela[contTab] =  ["","CMEG",""]
         contTab++
       }
+      
       tabela[contTab] =  ["","DSVF","L2"]
       contTab++
     }
+
+    if(varWhile==1&&cadeia[i][1]=='}'){
+      tabela[contTab] =  ["","DSVS","L1"]
+      contTab++
+      tabela[contTab] =  ["L2","NADA",""]
+      contTab++
+      varWhile=0
+    }
+//  0 ponto ====================================================================
+//  FIM
+  if(cadeia[i][1]=="}"&&fimElse==1){
+    tabela[contTab] =  ["L4","NADA",""]
+    contTab++
+    fimElse=null
+  }
+
+
+  if(cadeia[i][1]=="{"){
+    fim+=1
+  }
+  if(cadeia[i][1]=="}"){
+    fim-=1
+  }
+  if(fim==0){
+    tabela[contTab] =  ["","DMEM",variaveis.length]
+    contTab++
+    tabela[contTab] =  ["","PARA",""]
+    contTab++
+  }
 
     i++
   }
